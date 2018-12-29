7a871fed0082d6535d3831505baf9fb1a16241a4
Nitesh Paudel <lognitesh97@gmail.com>
2018-12-29 16:29:39 +0545

    spring-boot-crud-with-mongo start

---
+++ src/main/java/com/nitesh/practise/SpringBootCrudWithMongoApplication.java
@@ -0,0 +1,14 @@
+package com.nitesh.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudWithMongoApplication.class, args);
+	}
+
+}
+
